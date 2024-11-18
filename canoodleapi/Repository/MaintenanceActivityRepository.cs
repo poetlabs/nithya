958265f59b08a6298e9f91ffb8228cae83abeb98
Vishnu Mohan <vishnum467@gmail.com>
2024-11-18 15:10:30 +0530

    t

--- canoodleapi/Repository/MaintenanceActivityRepository.cs
+++ canoodleapi/Repository/MaintenanceActivityRepository.cs
@@ -90,7 +90,7 @@ namespace canoodleapi.Repository
                 int currentday = (int)DateTime.Now.Day;
                 DateTime CurrentDateTime = DateTime.Now;
 
-                string sql = "SELECT ca.mcStatusID as CompletedStatusID,ma.* FROM MaintenanceActivities ma left join CompletedActivities ca on ma.activityId=ca.activityId and ca.mcStatusID not in (11,12) WHERE (mcintervalid = 3 AND CAST(SpecificTime AS TIME) = @CurrentTime )" +
+                string sql = "SELECT ca.mcStatusID as CompletedStatusID,ma.* FROM MaintenanceActivities ma left join CompletedActivities ca on ma.activityId=ca.activityId and ca.mcStatusID not in (11,12) WHERE (mcintervalid = 3 AND  (DATEPART(day, '@CurrentDateTime') - DATEPART(day, (select top 1 updateddate from CompletedActivities where activityId=1 order by 1 desc))) = 1  ) " +
                     " OR (mcintervalid = 4 AND SpecificDayOfWeek = @CurrentDayOfWeek) OR (mcintervalid = 5 AND SpecificDayOfMonth = @currentday ) " +
                     " OR (mcintervalid = 6 AND (DATEPART(YEAR, @CurrentDateTime) - DATEPART(YEAR, (select top 1 updateddate from CompletedActivities where activityId=1 order by 1 desc))) = 1 AND " +
                     " SpecificDayOfMonth = @currentday) ";
