c695d88ccbc22770a8cc9c64485d2a6ec7dfe08d
Vishnu Mohan <vishnum467@gmail.com>
2024-11-26 11:56:41 +0530

    test

--- canoodleapi/Repository/MaintenanceActivityRepository.cs
+++ canoodleapi/Repository/MaintenanceActivityRepository.cs
@@ -107,7 +107,7 @@ namespace canoodleapi.Repository
                 int currentday = (int)DateTime.Now.Day;
                 DateTime CurrentDateTime = DateTime.Now;
 
-                string sql = "SELECT ca.mcStatusID as CompletedStatusID,ca.CompletionId as CompletionId,m.name as MachineName,ma.* FROM MaintenanceActivities ma left join CompletedActivities ca on ma.activityId=ca.activityId and ca.mcStatusID not in (11,12) inner join Machines m on ma.machineid=m.machineid WHERE (mcintervalid = 3 AND  (DATEPART(day, @CurrentDateTime) - DATEPART(day, (select top 1 updateddate from CompletedActivities  where activityId=ma.activityId order by 1 desc))) = 1  ) " +
+                string sql = "SELECT ca.mcStatusID as CompletedStatusID,ca.CompletionId as CompletionId,m.name as MachineName,ma.* FROM MaintenanceActivities ma left join CompletedActivities ca on ma.activityId=ca.activityId and ca.mcStatusID  in (9) inner join Machines m on ma.machineid=m.machineid WHERE (mcintervalid = 3 AND  (DATEPART(day, @CurrentDateTime) - DATEPART(day, (select top 1 updateddate from CompletedActivities  where activityId=ma.activityId order by 1 desc))) = 1  ) " +
                     " OR (mcintervalid = 4 AND SpecificDayOfWeek = @CurrentDayOfWeek) OR (mcintervalid = 5 AND SpecificDayOfMonth = @currentday ) " +
                     " OR (mcintervalid = 6 AND (DATEPART(YEAR, @CurrentDateTime) - DATEPART(YEAR, (select top 1 updateddate from CompletedActivities where activityId=ma.activityId order by 1 desc))) = 1 AND " +
                     " SpecificDayOfMonth = @currentday) ";
