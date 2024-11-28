13570d2e72df98002c4801626ea2a41e9cc89571
Vishnu Mohan <vishnum467@gmail.com>
2024-11-28 19:46:57 +0530

    c

--- canoodleapi/Repository/CompletedActivityRepository.cs
+++ canoodleapi/Repository/CompletedActivityRepository.cs
@@ -290,7 +290,7 @@ namespace canoodleapi.Repository
             try
             {
                 List<CompletedActivities> lstcompactivites = new List<CompletedActivities>();
-                string sql = "select m.name as MechineName,Mc.mcommonname as StatusName,Ma.descriptions as Descriptions,C.* from CompletedActivities C inner join LaborerVisits L on C.VisitID=L.VisitID " +
+                string sql = "select m.name as MachineName,Mc.mcommonname as StatusName,Ma.descriptions as Descriptions,C.* from CompletedActivities C inner join LaborerVisits L on C.VisitID=L.VisitID " +
                     " inner join LaborerLogin LL on L.laborerloginid=LL.laborerloginid " +
                     " inner join MaintenanceActivities Ma on c.ActivityID=Ma.ActivityID " +
                     " inner join Machines m on Ma.machineid=m.machineid " +
